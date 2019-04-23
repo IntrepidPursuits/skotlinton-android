b36ff2d75d14cbd80ffc4706d6ea4d5fbcc3a9a7
David Zou <david2@intrepid.io>
2019-04-23 14:24:25 -0400

    Take a screenshot whenever an UI test failed

--- app/src/androidTest/kotlin/io/intrepid/skotlinton/testutils/BaseUiTest.kt
+++ app/src/androidTest/kotlin/io/intrepid/skotlinton/testutils/BaseUiTest.kt
@@ -1,19 +1,59 @@
 package io.intrepid.skotlinton.testutils
 
+import android.os.Environment
+import androidx.test.espresso.Espresso
+import androidx.test.runner.screenshot.BasicScreenCaptureProcessor
+import androidx.test.runner.screenshot.ScreenCaptureProcessor
+import androidx.test.runner.screenshot.Screenshot
 import io.intrepid.skotlinton.InstrumentationTestApplication
 import org.junit.After
+import org.junit.Before
 import org.junit.Rule
+import org.junit.rules.TestName
 import org.mockito.junit.MockitoJUnit
 import org.mockito.junit.MockitoRule
+import java.io.File
 
 abstract class BaseUiTest {
     @Rule
     @JvmField
     val mockitoRule: MockitoRule = MockitoJUnit.rule()
+    @Rule
+    @JvmField
+    val testNameRule = TestName()
+
+    private lateinit var screenCaptureProcessor: ScreenCaptureProcessor
+
+    @Before
+    fun baseSetUp() {
+        screenCaptureProcessor = SpoonScreenCaptureProcessor(this::class.java.name, testNameRule.methodName)
+        // Take a screenshot whenever a test fails. The screenshot can be viewed in the spoon report or manually at
+        // the phone's /sdcard/app_spoon-screenshots/ directory.
+        Espresso.setFailureHandler { error, _ ->
+            takeScreenshot("test_failure")
+            throw error
+        }
+    }
 
     @After
-    fun tearDown() {
+    fun baseTearDown() {
         InstrumentationTestApplication.clearRestApiOverride()
         InstrumentationTestApplication.clearUserSettingsOverride()
+    }
+
+    fun takeScreenshot(screenShotName: String) {
+        val screenCapture = Screenshot.capture()
+        screenCapture.name = screenShotName
+        screenCaptureProcessor.process(screenCapture)
+    }
+}
+
+// Saves the screenshot to the directory that is expected by the spoon report.
+class SpoonScreenCaptureProcessor(className: String, methodName: String) : BasicScreenCaptureProcessor() {
+    init {
+        val spoonDirectory = File(Environment.getExternalStorageDirectory(), "app_spoon-screenshots")
+        val classDirectory = File(spoonDirectory, className)
+        val screenshotDirectory = File(classDirectory, methodName)
+        this.mDefaultScreenshotPath = screenshotDirectory
     }
 }
